b6f704fe36782c8aa385aee7a03483f6c1066d92
mizuki.sonoko <mizuki.sonoko@gmail.com>
2015-12-14 16:45:58 +0900

    [Init] add token class

---
+++ token.h
@@ -0,0 +1,47 @@
+#include <string>
+
+class Token{
+	  public:
+		enum Type{
+	        ERROR = -1,
+	        MARGIN,
+	        FIN,
+	        NAME,
+	        NUMBER,
+	        REALNUMBER,
+	        IDENTIFIER,
+	        EXCLAMATION,
+	        LPARENT,
+	        RPARENT,
+	        RBRACKET,
+	        LBRACKET,
+	        RABRACKET,
+	        LABRACKET,
+	        SEMICOLON,
+	        COLON,
+	        COMMA,
+	        PERIOD,
+	        CARET,
+	        AT_SIGN,
+	        DQUOTATION,
+	        EQUAL,
+	        OPE_ADD, 
+	        OPE_SUB, 
+	        OPE_MUL, 
+	        OPE_DIV, 
+		};
+
+		Token(Type aType,std::string aValue):
+		    type_(aType),
+		    value_(aValue){}
+
+		Type type(){
+			return type_;
+		}
+		std::string value(){
+			return value_;
+		}
+	  private:
+		Type type_;
+		std::string value_;
+	};
