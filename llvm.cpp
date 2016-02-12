ce30e87de7f9ad575eea9751153f903957441404
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-02-13 00:40:35 +0900

    [Add] function

--- llvm.cpp
+++ llvm.cpp
@@ -12,39 +12,66 @@
 #include <llvm/IR/IRPrintingPasses.h>
 #include <llvm/Support/raw_ostream.h>
 
+#include <string>
+#include <memory>
+
 namespace codegen{
 
-template<typename T>
+    using namespace std;
+
     struct CodeGenContext{
-        llvm::LLVMContext context;
-        llvm::Module const module;
+        llvm::LLVMContext& context;
+        llvm::Module* module;
+        llvm::IRBuilder<> builder;
+        CodeGenContext():
+            context(llvm::getGlobalContext()),
+            module(new llvm::Module(move("default"), context )),
+            builder(context)
+        {}
 
-        CodeGenContext(string moduleName){
-            context = llvm::getGlobalContext();
-        }
-
+        CodeGenContext(string moduleName):
+            context(llvm::getGlobalContext()),
+            module(new llvm::Module( move(moduleName), context )),
+            builder(context)
+        {}
 
     }; 
 
-    shared_ptr<CodeDenContext> context; 
-
-    llvm::Function* makeFunction<void>(string  name){
-      return  llvm::Function::Create(
-          llvm::FunctionType::get( builder.getVoidTy(), false ),
-          llvm::Function::ExternalLinkage,
-          name,
-          module.get()
-        );
-    }
+    shared_ptr<CodeGenContext> context;
 
     template<typename T>
-    llvm::Function* makeFunction(){
+    llvm::Function* makeFunction(string  name){
       return nullptr;
     }
 
-}
-int main(){
+    template<>
+    llvm::Function* makeFunction<int>(string  name){
+      return  llvm::Function::Create(
+          llvm::FunctionType::get( context->builder.getInt32Ty(), false ),
+          llvm::Function::ExternalLinkage,
+          name,
+          context->module
+        );
+    }
 
+
+    template<>
+    llvm::Function* makeFunction<void>(string  name){
+      return  llvm::Function::Create(
+          llvm::FunctionType::get( context->builder.getVoidTy(), false ),
+          llvm::Function::ExternalLinkage,
+          name,
+          context->module
+        );
+    }
+
+    void init(){
+        context = make_shared<CodeGenContext>("sharo");
+    }
+
+};
+
+int main(){
 
     return 0;
   }
